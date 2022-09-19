ddeb25fedbcf29e324ad3c4b70a9ac4e9f32973c
Danyal <danyalmirza@gmail.com>
2022-09-19 16:36:37 +0200

    new stuff

--- projekt/doublePendelum.py
+++ projekt/doublePendelum.py
@@ -1,8 +1,12 @@
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-import typing
+from matplotlib.patches import Circle
+import matplotlib.animation as animation
+from collections import deque
 
+# CONSTANTS
 g = 9.81
 m = 1
 l = 1
@@ -27,7 +31,7 @@ def ode(t,y):
 #------------- SOLVERS ----------------
 
 # Euler
-def euler1(f,tspan,u0,dt):
+def euler(f,tspan,u0,dt):
     interval=round((tspan[1]-tspan[0])/dt)
     tvec=np.linspace(tspan[0],tspan[1],interval+1)
     u=np.zeros((len(tvec),len(u0)))
@@ -40,7 +44,7 @@ def euler1(f,tspan,u0,dt):
     return tvec, u
 
 
-
+# Runge Kutta
 def RK4(fun, tspan, u0, dt):
     interval=round((tspan[1]-tspan[0])/dt)
     tvec=np.linspace(tspan[0],tspan[1],interval+1)
@@ -56,34 +60,139 @@ def RK4(fun, tspan, u0, dt):
         i+=1
     return tvec, u
 
+
+
+
 # Initialvillkor
 Theta1_0, Theta2_0 = np.pi/10, np.pi/10
 p1_0, p2_0 = 0, 0
 S_0 = [Theta1_0, Theta2_0, p1_0, p2_0]
 tspan = [0, 10]
-dt = 0.05
+dt = 0.001
 
 #sol = solve_ivp(dSdt, t_span=[0, 100], y0=S_0)
-tvec, y = euler1(dSdt, tspan, S_0, dt)
-tvec1, u = RK4(dSdt, tspan, S_0, dt)
+#tvec, y = euler(dSdt, tspan, S_0, dt)
+tvec, y = RK4(dSdt, tspan, S_0, dt)
 
 # Theta1_sol = sol.t[0]
 # Theta2_sol = sol.t[1]
 # p1_sol = sol.t[2]
 # p2_sol = sol.t[3]
 
+# # Unpack z and theta as a function of time
+theta1, theta2 = y[:,0], y[:,2]
 
-print(y[:,0])
+history_len = 500  # how many trajectory points to display
+
+# Convert to Cartesian coordinates of the two bob positions.
+x1 = l * np.sin(theta1)
+y1 = -l * np.cos(theta1)
+x2 = x1 + l * np.sin(theta2)
+y2 = y1 - l * np.cos(theta2)
 
 
+
+
+fig = plt.figure()
+ax = fig.add_subplot(autoscale_on=False, xlim=(-l-l, l+l), ylim=(-l-l, 1.)) 
+ax.set_aspect('equal')
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+trace, = ax.plot([], [], '.-', lw=1, ms=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+history_x, history_y = deque(maxlen=history_len), deque(maxlen=history_len)
+
+
+def animate(i):
+    thisx = [0, x1[i], x2[i]]
+    thisy = [0, y1[i], y2[i]]
+
+    if i == 0:
+        history_x.clear()
+        history_y.clear()
+
+    history_x.appendleft(thisx[2])
+    history_y.appendleft(thisy[2])
+
+    line.set_data(thisx, thisy)
+    trace.set_data(history_x, history_y)
+    time_text.set_text(time_template % (i*dt))
+    return line, trace, time_text
+
+
+ani = animation.FuncAnimation(
+    fig, animate, len(y), interval=dt*1000, blit=True)
+
+plt.show()
+
+# # # Plotted bob circle radius
+# r = 0.05
+# # Plot a trail of the m2 bob's position for the last trail_secs seconds.
+# trail_secs = 1
+# # This corresponds to max_trail time points.
+# max_trail = int(trail_secs / dt)
+
+
+# def make_plot(i):
+#     # Plot and save an image of the double pendulum configuration for time
+#     # point i.
+#     # The pendulum rods.
+#     ax.plot([0, x1[i], x2[i]], [0, y1[i], y2[i]], lw=2, c='k')
+#     # Circles representing the anchor point of rod 1, and bobs 1 and 2.
+#     c0 = Circle((0, 0), r/2, fc='k', zorder=10)
+#     c1 = Circle((x1[i], y1[i]), r, fc='b', ec='b', zorder=10)
+#     c2 = Circle((x2[i], y2[i]), r, fc='r', ec='r', zorder=10)
+#     ax.add_patch(c0)
+#     ax.add_patch(c1)
+#     ax.add_patch(c2)
+
+#     # The trail will be divided into ns segments and plotted as a fading line.
+#     ns = 20
+#     s = max_trail // ns
+
+#     for j in range(ns):
+#         imin = i - (ns-j)*s
+#         if imin < 0:
+#             continue
+#         imax = imin + s + 1
+#         # The fading looks better if we square the fractional length along the
+#         # trail.
+#         alpha = (j/ns)**2
+#         ax.plot(x2[imin:imax], y2[imin:imax], c='r', solid_capstyle='butt',
+#                 lw=2, alpha=alpha)
+
+#     # Centre the image on the fixed anchor point, and ensure the axes are equal
+#     ax.set_xlim(-l-l-r, l+l+r)
+#     ax.set_ylim(-l-l-r, l+l+r)
+#     ax.set_aspect('equal', adjustable='box')
+#     plt.axis('off')
+#     plt.savefig('frames/_img{:04d}.png'.format(i//di), dpi=72)
+#     plt.cla()
+
+
+
+
+# # # Make an image every di time points, corresponding to a frame rate of fps
+# # # frames per second.
+# # # Frame rate, s-1
+# fps = 10
+# di = int(1/fps/dt)
+# fig = plt.figure(figsize=(8.3333, 6.25), dpi=72)
+# ax = fig.add_subplot(111)
+
+# for i in range(0, len(tvec), di):
+#     print(i // di, '/', len(tvec) // di)
+#     make_plot(i)
 #-------------- PLOTTING ----------------
-# vinklar EULER
-plt.plot(tvec,y[:,0], 'r')
-plt.plot(tvec,y[:,1], 'b')
+# # vinklar EULER
+# plt.plot(tvec,y[:,0], 'r')
+# plt.plot(tvec,y[:,1], 'b')
 
-# vinklar RK
-plt.plot(tvec,u[:,0], 'y')
-plt.plot(tvec,u[:,1], 'g')
+# # vinklar RK
+# plt.plot(tvec,u[:,0], 'y')
+# plt.plot(tvec,u[:,1], 'g')
 
 
 
@@ -92,4 +201,4 @@ plt.plot(tvec,u[:,1], 'g')
 # plt.plot(tvec,y[:,2], 'y')
 # plt.plot(tvec,y[:,3], 'g')
 
-plt.show()
+# plt.show()
